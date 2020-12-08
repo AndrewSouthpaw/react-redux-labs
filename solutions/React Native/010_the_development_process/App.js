e3042a3424deda80365e25bc8eb08ebb83f761dd
Andrew Smith <andrewsmith@alumni.stanford.edu>
2020-12-08 09:51:58 -0800

    Push up solution to lab 1.

--- solutions/React Native/010_the_development_process/App.js
+++ solutions/React Native/010_the_development_process/App.js
@@ -1,10 +1,12 @@
+import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Edit App.js in your ide to start working on your app!</Text>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
     </View>
   );
 }
