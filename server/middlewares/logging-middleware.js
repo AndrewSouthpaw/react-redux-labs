d0c61fa41040dc75ba10f2b160ed9afe8bcd06c7
Andrew Smith <andrewsmith@alumni.stanford.edu>
2020-12-15 20:45:44 -0800

    Add a slowed down server version for axios lab.

--- server/middlewares/logging-middleware.js
+++ server/middlewares/logging-middleware.js
@@ -1,8 +1,16 @@
 module.exports = (req, res, next) => {
- res.set('X-Powered-By', 'Agile Gadgets');
- console.log('*'.repeat(80))
-//  console.log(req.path)
-//  console.log(req.route)
-//  console.log('*'.repeat(80))
- next();
+  res.set('X-Powered-By', 'Agile Gadgets')
+  console.log('*'.repeat(80))
+  //  console.log(req.path)
+  //  console.log(req.route)
+  //  console.log('*'.repeat(80))
+  const slowdown = process.env.SLOWDOWN
+  if (slowdown) {
+    console.log(`Simulating a slow server with ${slowdown}ms latency`)
+    setTimeout(() => {
+      next()
+    }, slowdown)
+  } else {
+    next()
+  }
 }
