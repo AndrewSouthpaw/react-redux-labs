05150c2cc8bf6663fca954991bc3793c9cfac91a
Andrew Smith <andrewsmith@alumni.stanford.edu>
2020-12-17 08:56:55 -0800

    Fix server chaos monkey.

--- server/middlewares/chaos-monkey-failure.js
+++ server/middlewares/chaos-monkey-failure.js
@@ -1,27 +1,8 @@
 module.exports = (req, res, next) => {
-  if (applySlowdown(req, res, next)) return
-  if (applyFailures(req, res, next)) return
-  next()
-}
-
-const applySlowdown = (req, res, next) => {
-  const slowdown = process.env.SLOWDOWN
-  if (!slowdown) return
-
-  console.log(`Simulating a slow server with ${slowdown}ms latency`)
-  setTimeout(() => {
-    next()
-  }, slowdown)
-
-  return true
-}
-
-const applyFailures = (req, res, next) => {
   const failures = parseFloat(process.env.FAILURES || 0)
-  if (!failures) return
+  if (!failures) return next()
 
   console.log(`Simulating flaky server with ${failures * 100}% chance of failure`)
   if (Math.random() < failures) throw new Error('Server failure 😬')
   next()
-  return true
 }
